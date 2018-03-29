cf76077b7d5484ddefbdc172b8835d91ca919043
shailenpatel1 <shailenster@gmail.com>
2018-03-29 02:31:54 -0500

    random asteroids

--- GalaxyRacerz/GalaxyRacerz/GameViewController.swift
+++ GalaxyRacerz/GalaxyRacerz/GameViewController.swift
@@ -15,6 +15,12 @@ class GameViewController: UIViewController, SCNPhysicsContactDelegate {
     var ship = SCNNode()
     var asteroid = SCNNode()
     var scene = SCNScene()
+    var gameOver = false
+    let queue = DispatchQueue.global()
+    var date = Date()
+    var time = TimeInterval()
+    var spawnTime: TimeInterval = 0
+    var leftOrRight = false
     
     override func viewDidLoad() {
         
@@ -22,7 +28,7 @@ class GameViewController: UIViewController, SCNPhysicsContactDelegate {
         // create a new scene
         scene = SCNScene(named: "art.scnassets/ship.scn")!
         scene.physicsWorld.contactDelegate = self
-
+        
         //creates and adds camera node to scene
         createCameraAndLight()
         
@@ -32,24 +38,20 @@ class GameViewController: UIViewController, SCNPhysicsContactDelegate {
         // retrieve the ship node
         ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
         
-        //adds asteroid object
-        asteroid = createAsteroid(scene: scene)
-        
         ship.physicsBody = SCNPhysicsBody(type: .kinematic, shape: nil)
+        
+        //asteroid = createAsteroid(scene: scene)
+        
         // detects interaction between asteroids and ship
-        asteroid.physicsBody!.categoryBitMask = 2
-        ship.physicsBody!.categoryBitMask = 1 
-        asteroid.physicsBody!.contactTestBitMask = 1
-
+        ship.physicsBody!.categoryBitMask = 1
+//        asteroid.physicsBody!.categoryBitMask = 2
+//        asteroid.physicsBody!.contactTestBitMask = 1
+        
         // retrieve the SCNView
         let scnView = self.view as! SCNView 
         
         // set the scene to the view
         scnView.scene = scene
-        
-        //gesture
-        let moveBy = SCNAction.moveBy(x: 0, y: 1, z: 0, duration: 1)
-        ship.runAction(moveBy)
         
         // allows the user to manipulate the camera
         scnView.allowsCameraControl = true
@@ -66,6 +68,25 @@ class GameViewController: UIViewController, SCNPhysicsContactDelegate {
         
     }
     
+    var done = false
+    var contact = SCNPhysicsContact()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        if(!gameOver) {
+            queue.async {
+                while(!self.gameOver) {
+                    self.time = -self.date.timeIntervalSinceNow
+                    if(self.time > self.spawnTime) {
+                        DispatchQueue.main.async {
+                            self.asteroid = self.createAsteroid(scene: self.scene)
+                        }
+                        self.spawnTime = self.time + TimeInterval(arc4random_uniform(6) + 1);
+                    }
+                }
+            }
+        }
+    }
+    
     func createCameraAndLight() {
         //let cameraNode = SCNNode()
         let cameraNode = SCNNode()
@@ -77,7 +98,6 @@ class GameViewController: UIViewController, SCNPhysicsContactDelegate {
         // place the camera
         
         cameraNode.position = SCNVector3(x: 0, y: 10, z: 31)
-        //cameraNode.position = SCNVector3(x: 0, y: 0, z: 0)
         // create and add a light to the scene
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
@@ -94,20 +114,33 @@ class GameViewController: UIViewController, SCNPhysicsContactDelegate {
     }
     
     func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
-        print("HERE")
         if (contact.nodeA == ship || contact.nodeA == asteroid) && (contact.nodeB == ship || contact.nodeB == asteroid) {
             ship.removeFromParentNode()
+            gameOver = true
         }
     }
     
     func createAsteroid(scene: SCNScene) -> SCNNode {
-        let sphere = SCNSphere(radius: 3)
+        let sphere = SCNSphere(radius: 2)
         let sphereNode = SCNNode(geometry: sphere)
-        sphereNode.position = SCNVector3(0.0, 10.0, -20.0)
+        var xCoord = 0
+        if(leftOrRight) {
+            xCoord = Int(arc4random_uniform(8) + 1)
+        }
+        else {
+            xCoord = -1 * Int(arc4random_uniform(8))
+        }
+        leftOrRight = !leftOrRight
+        sphereNode.position = SCNVector3(Double(xCoord), 5.0, -60.0)
         let body = SCNPhysicsBody(type: .dynamic, shape: nil)
         sphereNode.physicsBody = body
-        sphereNode.physicsBody?.velocity = SCNVector3(0,0,18)
+        sphereNode.physicsBody?.velocity = SCNVector3(0, 0, 58)
         scene.rootNode.addChildNode(sphereNode)
+        
+        self.asteroid = sphereNode
+        self.asteroid.physicsBody!.categoryBitMask = 2
+        self.asteroid.physicsBody!.contactTestBitMask = 1
+        
         return sphereNode
     }
     
@@ -131,13 +164,13 @@ class GameViewController: UIViewController, SCNPhysicsContactDelegate {
             let location2D = gestureRecognize.location(in: scnView)
             
             //Location of the finger in a 3D vector
-            let location3D = SCNVector3Make(Float(location2D.x), Float(location2D.y), projectedOrigin.z)
+            let location3D = SCNVector3(Float(location2D.x), Float(location2D.y), projectedOrigin.z)
             
             //Unprojects a point from the 2D pixel coordinate system of the renderer to the 3D world coordinate system of the scene
             let realLocation3D = scnView.unprojectPoint(location3D)
             
-            //Only updating Y axis position
-            node.position = SCNVector3Make(realLocation3D.x, (node.position.y), (node.position.z))
+            //Only updating X axis position
+            ship.position = SCNVector3(realLocation3D.x, (node.position.y), (node.position.z))
             
             // get its material
             let material = result.node.geometry!.firstMaterial!
